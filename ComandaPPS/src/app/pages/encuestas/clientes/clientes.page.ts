215cfd58d6c7d7bd030efe3cec11b06df1624ec2
FacundoFalcioni2410 <facundofalcioni2410@gmail.com>
2021-10-19 19:33:23 -0300

    correccion de bug subir fotos producto

--- ComandaPPS/src/app/pages/encuestas/clientes/clientes.page.ts
+++ ComandaPPS/src/app/pages/encuestas/clientes/clientes.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Vibration } from '@ionic-native/vibration/ngx';
+import Swal from 'sweetalert2/src/sweetalert2.js'
 
 @Component({
   selector: 'app-clientes',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesPage implements OnInit {
 
-  constructor() { }
+  nombresFotos = []
+  formDataFotos: FormData = null;
+
+  constructor(private vibration: Vibration) { }
 
   ngOnInit() {
   }
@@ -15,5 +20,30 @@ export class ClientesPage implements OnInit {
   change(value){
     console.log(value.detail.value);
   }
+
+  mostrarToast(options: any){
+    Swal.fire(options);
+  }
+
+  archivoSeleccionado(event: any)
+  {
+    if (event.target.files.length === 3)
+    {
+      this.formDataFotos = new FormData();
+      for (let i = 0; i < 3; i++)
+      {
+        this.nombresFotos.push(event.target.files[i].name);
+        this.formDataFotos.delete(`archivo${i}`);
+        this.formDataFotos.append(`archivo${i}`, event.target.files[i], event.target.files[i].name);
+      }
+    }
+    else
+    {
+      this.formDataFotos = null;
+      this.vibration.vibrate(2000);
+      this.mostrarToast({text: "Debe seleccionar 3 fotos", toast: true, position: 'bottom',timer: 2000,timerProgressBar: true, icon: 'error'});
+    }
+  }
+
 
 }
