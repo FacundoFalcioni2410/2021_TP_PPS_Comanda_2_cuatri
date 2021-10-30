f00a2e8b1c687778959e888d70f54f4337ac873c
FacundoFalcioni2410 <facundofalcioni2410@gmail.com>
2021-10-29 21:45:52 -0300

    correccion bug en lista bartender

--- ComandaPPS/src/app/pages/empleados/lista-bartender/lista-bartender.page.ts
+++ ComandaPPS/src/app/pages/empleados/lista-bartender/lista-bartender.page.ts
@@ -65,43 +65,4 @@ export class ListaBartenderPage implements OnInit {
           })
       });
   }
-
-  init() {
-    document.querySelector(".start").addEventListener("click", this.cronometrar);
-    document.querySelector(".stop").addEventListener("click", this.parar);
-    document.querySelector(".reiniciar").addEventListener("click", this.reiniciar);
-    this.h = 0;
-    this.m = 0;
-    this.s = 0;
-    document.getElementById("hms").innerHTML = "00:00:00";
-  }
-  cronometrar() {
-    this.escribir();
-    this.id = setInterval(this.escribir, 1000);
-    document.querySelector(".start").removeEventListener("click", this.cronometrar);
-  }
-  escribir() {
-    var hAux, mAux, sAux;
-    this.s++;
-    if (this.s > 59) { this.m++; this.s = 0; }
-    if (this.m > 59) {this.h++; this.m = 0; }
-    if (this.h > 24) { this.h = 0; }
-
-    if (this.s < 10) { sAux = "0" + this.s; } else { sAux = this.s; }
-    if (this.m < 10) { mAux = "0" + this.m; } else { mAux = this.m; }
-    if (this.h < 10) { hAux = "0" + this.h; } else { hAux = this.h; }
-
-    document.getElementById("hms").innerHTML = hAux + ":" + mAux + ":" + sAux;
-  }
-  parar() {
-    clearInterval(this.id);
-    document.querySelector(".start").addEventListener("click", this.cronometrar);
-
-  }
-  reiniciar() {
-    clearInterval(this.id);
-    document.getElementById("hms").innerHTML = "00:00:00";
-    this.h = 0; this.m = 0; this.s = 0;
-    document.querySelector(".start").addEventListener("click", this.cronometrar);
-  }
 }
